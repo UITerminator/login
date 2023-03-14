bb0c2c8db946d12d65bcd55d12509872c45e0474
alirrah <alir.rah81@gmail.com>
2023-03-15 02:54:18 +0330

    add signup

---
+++ script.js
@@ -0,0 +1,14 @@
+let signup = document.querySelector(".signup__text");
+let login = document.querySelector(".login__text");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+ 
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+});
+ 
+login.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
